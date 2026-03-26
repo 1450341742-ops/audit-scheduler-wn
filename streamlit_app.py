1c2556b5d75c62c637e6ec1921d53dc2bdaf11d8
1450341742-ops <1450341742@qq.com>
2026-03-26 15:37:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import Session, joinedload
 from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
 
-from app.db import Base, SessionLocal, engine, ensure_schema, IS_SQLITE, IS_SQLITE, DB_URL
+from app.db import Base, SessionLocal, engine, ensure_schema, IS_SQLITE, DB_URL
 from app.models import Auditor, Task, Schedule, CityDistance, City
 from app.scheduler import (
     build_candidates,
